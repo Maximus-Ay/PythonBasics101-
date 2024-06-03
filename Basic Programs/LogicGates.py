c23cc970e4e71082d44832389bb42d09abcfdfda
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-03 10:03:18 -0700

    Create LogicGates.py

---
+++ Basic Programs/LogicGates.py
@@ -0,0 +1,7 @@
+'''
+    This program is an interactive program that permits me to couple my knowledge of conditional statements
+    and loops to represent the different logic gates.
+    Here I will be dealing mostly with the AND, OR and NOT gate.
+
+'''
+
