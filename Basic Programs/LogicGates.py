a551b671c26eaa60329925cb80f4bd613b12acc3
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-03 10:39:07 -0700

    Update LogicGates.py

--- Basic Programs/LogicGates.py
+++ Basic Programs/LogicGates.py
@@ -4,4 +4,42 @@
     Here I will be dealing mostly with the AND, OR and NOT gate.
 
 '''
+def Choice(choice):
+    pass
+
+def And(firstInput, secondInput):
+    pass
+def Or(firstInput, secondInput):
+    pass
+
+def Not(firstInput):
+    pass
+
+def Nand(firstInput, secondInput):
+    pass
+
+def Nor(firstInput, secondInput):
+    pass
+
+def Xor(firstInput, secondInput):
+    pass
+
+
+#MAIN 
+keepGoing = True
+
+while(keepGoing):
+    print("** WELCOME TO THE LOGIC GATE ANALOGY PROGRAM **")
+    print("1. AND GATE")
+    print("2. OR GATE")
+    print("3. NOT GATE")
+    print("4. NAND GATE")
+    print("5. NOR GATE")
+    print("6. XOR GATE")
+    print("7. Quit")
+
+    choice = int(input("Make a choice: "))
+
+
+
 
