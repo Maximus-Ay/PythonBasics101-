03a9531e5358620e7965df5d509cd95301ea646b
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-04 20:24:15 -0700

    Update Countdown.py

--- Basic Programs/Countdown.py
+++ Basic Programs/Countdown.py
@@ -2,3 +2,13 @@
     This is a basic python program that deals mostly with counting down based on user input.
     It makes use of the loops to be able to do that.
 '''
+
+
+
+number = int(input("Enter the number the count down should start from: "))
+
+while(number>=0):
+    print(number)
+
+    number-=1
+
