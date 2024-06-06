efff71ea3c8e6ec20d60b3a04709f1f847c52b6e
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-05 19:36:08 -0700

    Completion

--- Basic Programs/DivisorChecker.py
+++ Basic Programs/DivisorChecker.py
@@ -2,9 +2,14 @@
     This program checks to see if a number is a divisor of another.
 '''
 
-
+def isDivisor(number, check):
+    if number % check == 0:
+        return True
+    else:
+        return False
 
 number = int(input("Enter the number: "))
 
 check = int(input("Enter the number to check: "))
 
+print(isDivisor(number, check))
