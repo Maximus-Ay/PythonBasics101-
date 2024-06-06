96e3e380426c1b844efcb6f6879a796993d9130d
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-05 19:31:11 -0700

    Update MultipleChecker.py and Update DivisionChecker.py

---
+++ Basic Programs/DivisorChecker.py
@@ -0,0 +1,3 @@
+'''
+    This program checks to see if a number is a divisor of another.
+'''
--- Basic Programs/MultipleChecker.py
+++ Basic Programs/MultipleChecker.py
@@ -1,3 +1,7 @@
 '''
     This program aims at checking if a certain number is a multiple of another.
 '''
+
+number = int(input("Enter the number: "))
+
+multiple = int(int("Enter the number to check: "))
