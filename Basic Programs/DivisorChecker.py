2e7da410058ad1bcc1b9c51980c7909aa790ea07
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-05 19:33:27 -0700

    Update DivisorChecker.py defining variables

--- Basic Programs/DivisorChecker.py
+++ Basic Programs/DivisorChecker.py
@@ -1,3 +1,10 @@
 '''
     This program checks to see if a number is a divisor of another.
 '''
+
+
+
+number = int(input("Enter the number: "))
+
+check = int(input("Enter the number to check: "))
+
