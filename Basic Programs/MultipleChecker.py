547fb2cc9f60ce0f4cc855d5c9a4c62535f45d1c
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-06 20:10:16 -0700

    Update MultipleChecker.py

--- Basic Programs/MultipleChecker.py
+++ Basic Programs/MultipleChecker.py
@@ -1,11 +1,16 @@
 '''
     This program aims at checking if a certain number is a multiple of another.
 '''
-def multipleCheck(number):
-    pass
+def isMultiple(number, multiple):
+    if multiple % number == 0:
+        return True
+    else:
+        return False
 
 
 number = int(input("Enter the number: "))
 
 multiple = int(int("Enter the number to check: "))
+
+print(isMultiple(number, ))
 
