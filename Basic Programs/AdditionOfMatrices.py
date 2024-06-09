2855fac3eaab66117f42817cd4ac9896275ba8a0
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-09 05:16:08 -0700

    Create AdditionOfMatrices.py

---
+++ Basic Programs/AdditionOfMatrices.py
@@ -0,0 +1,7 @@
+'''
+    This is a simple python program that adds two matrices together.
+    Normally a matrix is a two dimwnsional array but in python we don't
+    have the array data structure, hence we will use the list data type or structure.
+
+'''
+
