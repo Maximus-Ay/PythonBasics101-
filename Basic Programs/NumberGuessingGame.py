de312c04556a4740b3db4462b2565b22f878d92e
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-09 04:59:48 -0700

    Update NumberGuessingGame.py

--- Basic Programs/NumberGuessingGame.py
+++ Basic Programs/NumberGuessingGame.py
@@ -12,25 +12,26 @@ max = int(input("Enter the max number: "))
 min = int(input("Enter the minimum number: "))
 attempts = int(input("Enter the number of attempts you want for the guessing: "))
 
-computerValue = random.randint(min, max+1)
+computerValue = random.randint(min, max)
 flag = True
 while flag == True:
-    userGuess = int(input("Enter your guess: "))
+    
+    if attempts > 0:
+        userGuess = int(input("Enter your guess: "))
 
-    if attempts == 0:
-        print("You failed the game!")
-        flag = False
-    else: 
         if userGuess > computerValue:
             attempts = attempts - 1
-            print("Too High. Try Again! you have {} attempts left".format(attempts))
+            print("Too High. Try Again! you have {} attempt(s) left".format(attempts))
+            
             
 
         elif userGuess < computerValue:
             attempts = attempts - 1
-            print("Too Low. Try Again! you have {} attempts left".format(attempts))
-
-
+            print("Too Low. Try Again! you have {} attempt(s) left".format(attempts))
+            
         else:
             print("Your guess is right!")
             flag = False
+    else:
+        print("You failed the game!")
+        flag = False
