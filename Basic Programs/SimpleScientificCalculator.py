c57bb90b97496d2b8bfc7e8935e3ab654a27d4d7
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-10 10:22:40 -0700

    Create SimpleScientificCalculator.py

---
+++ Basic Programs/SimpleScientificCalculator.py
@@ -0,0 +1,18 @@
+'''
+    This program is a simple scientific calculator and will enable me to be able to be able
+    to compress all basics of python needed to solve mathematical problems.
+    The calculator will begin with a menu and will allows users choose and based on the menu, they
+    will perform the different operation based on what the user wants.
+    This calculator will perform 
+    -> basic mathematical operations, 
+    -> operations on Matrices,
+    -> Vectors operations,
+    -> solving simultaneous equations with two unknowns
+    -> Solving simultaneous equations with three unknowns
+    -> A small calculation game at the end of it all.
+'''
+
+
+
+
+print("*** WELCOME TO MY BASIC CALCULATOR***")
