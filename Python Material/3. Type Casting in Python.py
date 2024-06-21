0a7945cd01b9076a958f8718de6a40726202f980
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-21 15:44:40 -0700

    Create 3. Type Casting in Python.py

---
+++ Python Material/3. Type Casting in Python.py
@@ -0,0 +1,15 @@
+'''
+    Type Casting is the art of converting a variable from one data type to another data type.
+    We have 4 data types which are Integers, String, Boolean and Floats.
+    Hence, with type casting you can move from one of these data types to another.
+    We have two types of casting, that is Implicit casting and Explicit casting
+
+    Explicit type casting occurs when you manually decide to cast it your self, where as implicit 
+    type casting is done automatically.
+
+    Below are some examples
+
+
+'''
+
+
