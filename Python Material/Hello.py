4ea59d2c8e8438dc169ba4aa65fb32d7f9902eee
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-21 14:35:54 -0700

    Intro to python

---
+++ Python Material/Hello.py
@@ -0,0 +1,12 @@
+# This is a comment in Python, It is not executed by the interpreter.
+
+'''
+    This is a multiline comment and 
+    it is not executed by the interpreter
+'''
+
+# To print a message in python, you use the print statement.
+
+print("Hello World")
+print("My Name is Maximus")
+print("This is my first Python program Hahaha")
