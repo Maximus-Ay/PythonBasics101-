6132dc11b7082971ff99b74905c466c00764c0ce
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-24 09:54:52 -0700

    Update 5. Math and arithmetic operators.py

--- Python Material/5. Math and arithmetic operators.py
+++ Python Material/5. Math and arithmetic operators.py
@@ -1,7 +1,39 @@
 '''
     Here we are going to see some functions in Mathematical module of python
+    we have the 4 basic:
+    Addition : +
+    Subtraction: -
+    Multiplication: *
+    Division: /
+
+    Others include the:
+    Modulus : % 
+    Exponent: **
+
 
 '''
+
+number = 12
+
+number = number + 1 # Increment number by 1
+number += 1 # Shortened form to increment number by 1. Augmented increment
+
+number = number - 1 # reduce number by 1
+number -= 1 # Shortened form to reducing number by 1.
+
+number = number * 2 # multiply number by 2
+number *= 2 # Shortened form
+
+number = number / 3 # divide number by 3
+number /= 1 # Shortened form 
+
+number = number % 4 # Increment number by 1
+number %= 1 # Shortened form 
+
+number = number ** 1 # Increment number by 1
+number **= 1 # Shortened form 
+
+
 
 
 
