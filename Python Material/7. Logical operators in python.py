52f59fa19208e5f8363b4301e223356e2b4ce238
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-26 08:48:51 -0700

    Update 7. Logical operators in python.py

--- Python Material/7. Logical operators in python.py
+++ Python Material/7. Logical operators in python.py
@@ -28,6 +28,39 @@ elif mark >= 40 and mark<=59:
 else:
     print("Try again next time, you didn't make it")
 
-# This simply demonstrates the use of the and operator, both of expressions must be true for it to function.
+# This simply demonstrates the use of the and operator, both of conditions must be true for it to function.
+
+# The and operator is mostly used when you are checking for ranges.
+
+
+# THE OR OPERATOR
+
+temp = 25
+#temp = -24
+
+if temp <=0 or temp <=30:
+    print("The temperature is bad!")
+else:
+    print("The temperature is good!")
+
+# This will output the temperature is bad because at least one condition is satisfied in the expression
+# The temperature is 25 which is not less than 0 but less than 30, hence it executes that which is in the 
+# if statement. If the temp was also -24 it will work.
+
+# THE NOT
+
+# The not statement basically finds the opposite of the statement in which you are using it.
+
+Integer = True
+
+if not Integer:
+    print("It is an integer but the not statement has converted it into something different!")
+else:
+    print("It is not an integer")
+
+
+
+
+
 
 
