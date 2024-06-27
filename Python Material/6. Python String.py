c84e1e680fc91c258215ce12131bfe4fd959808c
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-27 07:19:12 -0700

    Create 6. Python String.py

---
+++ Python Material/6. Python String.py
@@ -0,0 +1,25 @@
+'''
+    PYTHON STRINGS INTRODUCTIOn
+    => Strings in python are just a series of characters.
+    => They can be used to represent people's names and things like that as well as they
+       can contain a mixture of text and numbers
+    => You can declare a string by using " " quotation marks
+'''
+
+# Example declaring an empty string
+name = " "
+print(name)
+
+# This is a string
+
+hobby = "Basketball"
+print(hobby)
+
+# It can also contain a mixture of letters and characters.
+password = "TheTrue@234%290!"
+
+print(f"Your password is {password}")
+
+'''
+    
+'''
