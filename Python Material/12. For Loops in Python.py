14764ba461df530a5515cfe955397fc63668e2dd
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-28 10:37:52 -0700

    Create 12. For Loops in Python.py

---
+++ Python Material/12. For Loops in Python.py
@@ -0,0 +1,58 @@
+'''
+        PYTHON FOR LOOP
+    => A for loop is the best loop to use when you know the number of times you want to execute a particular
+       piece of code.
+    => You can iterate over a range, over a string, sequence etc.
+    => The simple for loop has a syntax of 
+              for counter in range(start, end, step):
+                    # Code to be executed.
+    => The range function helps you to do the action the number of times required and it has 3 parameters.
+    => The start is where it begins and it is inclusive, just like the indexing operator.
+    => The end is where it ends and it is exclusive just like the indexing operator.
+    => The step is the number of times you will skip for the next iteration.
+    => To reverse the movement of the range, you can use the reversed method over the range.
+
+'''
+
+# Let's print numbers from one to 10 using a for loop
+
+for x in range(1,11):
+    print(x, end=" ")
+
+# Here the end method is used to print all the 
+# numbers on a single line, since the print skips a line for all of them.
+# As we said to print from 1 to 10, te end needs to be 11, since the end is exclusive.
+
+
+# So print in reverse all we do is
+print("\nReverse the printing: ")
+for x in reversed(range(1,11)):
+    print(x, end=" ")
+
+
+# You can also iterate through strings.
+
+myName = "Maximus"
+print("\nLet's print each letter in Maximus separately using a for loop: ")
+for x in myName:
+    print(x, end="")
+
+print("\nMy name reversed is: ")
+for x in reversed(myName):
+    print(x, end="")
+
+
+# Making use of the step, we have, 
+print("Let's print from 1 to 20 in steps of 3: ")
+for x in range(1,21,3):
+    print(x, end=" ")
+
+
+
+
+
+
+
+
+
+
