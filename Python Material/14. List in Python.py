3548446c4cf94ea44910abcd752e018bc569765c
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-29 11:39:23 -0700

    Create 14. List in Python.py

---
+++ Python Material/14. List in Python.py
@@ -0,0 +1,11 @@
+'''
+    LIST IN PYTHON
+    => List are a what we call in python collections.
+    => collections are a single "variable" that can store multiple values.
+    => Other collections include: tuple, sets and dictionaries.
+    => Here we will focus on lists in python
+
+
+
+
+'''
