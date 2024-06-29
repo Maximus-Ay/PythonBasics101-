d2b62e04cc4c7903259e95fb83d42cec96a5dbbb
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-29 11:50:25 -0700

    Update 14. List in Python.py

--- Python Material/14. List in Python.py
+++ Python Material/14. List in Python.py
@@ -4,8 +4,19 @@
     => collections are a single "variable" that can store multiple values.
     => Other collections include: tuple, sets and dictionaries.
     => Here we will focus on lists in python
-
-
+    => To declare a list, you simply 
+        name_of_list = []
+    => Your list items are placed within the squared brackets.
+    => Some characteristics of list is that:
+        1. They are ordered.
+        2. They are changeable.
+        3. They can contain duplicates.
+    => Some of this characteristics, distinguishes it from other collections.
+    => You can iterate over a list.
+    => You can use the indexing operator to go through the elements of a list.
+    => To get all the properties of a list, you use the dir() function. dir(name_of_list)
+    => Also to get a more detailed properties of the list, you can use the help(name_of_list) function
+    => We will treat all the aspects of the list below.
 
 
 '''
