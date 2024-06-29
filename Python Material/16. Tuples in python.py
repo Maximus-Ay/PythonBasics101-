938478d1332d4e9cce63842c23cfba436962de86
Maximus-Ay <thedevelopermax@gmail.com>
2024-06-29 12:48:02 -0700

    Create 16. Tuples in python.py

---
+++ Python Material/16. Tuples in python.py
@@ -0,0 +1,13 @@
+'''
+    PYTHON TUPLES
+    => They are one of the collections besides list and sets.
+    => They are declared by using parenthesis:
+    => Example: 
+        name_of_tuple = () # This is an empty tuple
+    => Onike other collections,
+        1. Tuples are ordered, That is you can access them using indexing.
+        2. They are immutable, that is unchangeable, its values cannot be changes after being created.
+        3. They can contain duplicates as well.
+    => They do not have many methods as the other collections.
+    
+'''
