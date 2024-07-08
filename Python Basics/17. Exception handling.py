7e6e4b213f97449f6f990d08743f96c001b79cd8
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-07 19:58:25 -0700

    Update 17. Exception handling.py

--- Python Basics/17. Exception handling.py
+++ Python Basics/17. Exception handling.py
@@ -3,7 +3,28 @@
     => An exception is an event that occurs during the execution of a program that troubles the 
        flow of a program
     => Like for example, dividing a number by 0
+    => A simple exception is handled by using the try except block.
+    => The try block will attempt to do run the code that you talked of and if an error occurs 
+       it moves to the except section, but its is not good practice to use just one except for all exceptions
+       cause, what might be halting the program may be a particular error and another error manifested that
+       should not stop the program.
 
 
 
 '''
+
+# Using a simple try except block
+try:
+    number = float(input("Enter a number: "))
+    number_to_divide_by = float(input("Enter the number to divide by: "))
+    result = number/number_to_divide_by
+    print(result)
+
+#except Exception:
+    #print("Something went wrong")
+# So rather than using the conventional except Exception: you have to specify the type of error.
+# Some error include: ZeroDivisionError: When a number is divided by 0
+# Other include: ValueError: when you attempt to divide a number by a non number
+
+except ZeroDivisionError:
+    print("You cannot divide by zero! :(")
