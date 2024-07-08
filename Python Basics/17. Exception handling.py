673cfefaeca542bd3a7eccac273a1483c472d315
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-07 19:44:58 -0700

    Create 17. Exception handling.py

---
+++ Python Basics/17. Exception handling.py
@@ -0,0 +1,9 @@
+'''
+    EXCEPTION HANDLING
+    => An exception is an event that occurs during the execution of a program that troubles the 
+       flow of a program
+    => Like for example, dividing a number by 0
+
+
+
+'''
