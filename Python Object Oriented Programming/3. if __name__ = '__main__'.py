bc48bacd036c195bc95451303c8fc33c13fba97e
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-11 08:43:13 -0700

    Create 3. if __name__ = '__main__'.py

---
+++ Python Object Oriented Programming/3. if __name__ = '__main__'.py
@@ -0,0 +1,17 @@
+'''
+    the if __name__ = 'main'
+    This is a dunder method
+    Many attimes it is better to create a main method that will halp you structure your code
+
+'''
+
+def main():
+    print("This is my main code!!")
+
+
+if __name__ == "__main__":
+    main()
+    
+# the if __name__ == "__main__", in case the where you are running the python program directly
+#  and avoids the situation where you run some global variables, that are not needed
+
