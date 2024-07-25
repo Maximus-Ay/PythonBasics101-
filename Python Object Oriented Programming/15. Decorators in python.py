8fe98c0f1eef32d39b3001b5067eee134ce71eb3
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-24 19:24:54 -0700

    Decorators in python

---
+++ Python Object Oriented Programming/15. Decorators in python.py
@@ -0,0 +1,11 @@
+'''
+    DECORATORS IN PYTHON
+    => a decorator in python is a function that extends the behaviour of another function with or without 
+       modifying the base function
+    => pass the base function as an argument to the decorator.
+    => 
+       @add_sprinkles
+       get_ice_cream("Vanilla")
+
+'''
+
