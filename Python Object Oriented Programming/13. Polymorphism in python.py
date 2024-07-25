fb601731890e8e13e318cbc483a96e116998c93b
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-24 18:37:31 -0700

    Introduction to polymorphism

---
+++ Python Object Oriented Programming/13. Polymorphism in python.py
@@ -0,0 +1,17 @@
+'''
+    POLYMORPHISM IN PYTHON
+    => Polymorphism is word that originates from two greek words called: 
+                   Poly = Many
+                   Morphe = Forms
+    => Hence Polymorphism means to have many forms or faces.
+    => Polymorphism can be achieved in python using two concepts:
+       1) Inheritance
+       2) Duck typing
+    => Here we will focus more on inheritance
+'''
+
+# HOW POLYMORPHISM IS ACHIEVED USING INHERITANCE
+# Let;s say we have a parent class Shape and child classes: Square, Cicrle, Triangle
+# when you create a cicle object it acts as both a circle and a shape since it inherits from 
+# the Shape class, hence it has two forms and so polymorphism is achieved.
+
