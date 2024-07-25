ee9f8099abc3744b3d92fc9604e54c022fe81946
Maximus-Ay <thedevelopermax@gmail.com>
2024-07-24 19:18:20 -0700

    Abstract classes in python

--- Python Object Oriented Programming/14. Abstract classes in python.py
+++ Python Object Oriented Programming/14. Abstract classes in python.py
@@ -9,8 +9,31 @@
        inherited abstract methods.
     => To start dealing with abstract classes in python we need to use the abc module which is 
        short form for abc = Abstract base class, we could equally import abstract method too
+    => When a class inherits from the abstract class, that contains abstract methods, it must provide an 
+       implementation for all those methods.
 
 
 '''
 
 from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def area(self):
+        print("The area of the circle is null")
+
+
+class Triangle(Shape):
+    def area(self):
+        print("This is the area of the triangle")
+
+triangle = Triangle()
+circle = Circle()
+
+circle.area()
+triangle.area()
+
