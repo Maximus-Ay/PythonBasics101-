7ba78e1b7d7014d0e0dbedaedf466d4e252c147f
Maximus-Ay <thedevelopermax@gmail.com>
2024-08-02 08:55:52 -0700

    Create 17. Duck typing in python.py

---
+++ Python Object Oriented Programming/17. Duck typing in python.py
@@ -0,0 +1,7 @@
+'''
+    DUCK TYPING IN PYTHON
+    => Duck typing is another way to achieve polymorphism besides inheritance
+    => The simple analogy here is that: if it looks like a duck, and it quacks like a duck, then it must be a duck
+    => Objects must have the minimum necessary attributes/methods
+
+'''
