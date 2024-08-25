3a91bfe5a74d48e52627e98e970b56990dfc63ea
Maximus <164879113+Maximus-Ay@users.noreply.github.com>
2024-08-25 21:04:47 +0100

    Update AdditionOfMatrices.py

--- Basic Programs/AdditionOfMatrices.py
+++ Basic Programs/AdditionOfMatrices.py
@@ -2,7 +2,7 @@
     This is a simple python program that adds two matrices together.
     Normally a matrix is a two dimwnsional array but in python we don't
     have the array data structure, hence we will use the list data type or structure.
-    Note that to add two matrices, they must have the same number of rows and columns
+    Note that to add two matrices, they must have the same number of rows and columns, if not it won't be possible to perform such an operation.
 
 '''
  
